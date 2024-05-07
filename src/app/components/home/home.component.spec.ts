5039787f1dc1c5a1420e0eaa647ecddaaeb742e9
Judit Vasko-Szedlar <129600562+jvaskone@users.noreply.github.com>
2024-05-07 15:17:19 +0200

    unit testing HomeComponent

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule} from '@angular/common/http/testing';
 import { HomeComponent } from './home.component';
+import { ThemeSwitcherService } from '../../services/theme-switcher.service';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +9,10 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent, HttpClientTestingModule]
+      imports: [HomeComponent, HttpClientTestingModule],
+      providers: [
+        {provide: ThemeSwitcherService, useValue: { isDarkTheme: () => false }}
+      ]
     })
     .compileComponents();
     
@@ -20,4 +24,38 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display the selectedSortBy label', () => {
+    component.selectedSortBy = 'Area';
+    fixture.detectChanges();
+    const label = fixture.nativeElement.querySelector('#sort-by-label');
+    expect(label.textContent).toContain('Area');
+  });
+
+  it('should open the dropdown when the icon is clicked', () => {
+    // Arrange: Retrieve the dropdown icon element
+    const iconElement = fixture.nativeElement.querySelector('#selectElement');
+
+    // Act: Simulate clicking the icon
+    iconElement.click();
+    fixture.detectChanges();
+
+    // Assert: Check if the dropdown is open (e.g., by querying options)
+    const dropdownOptions = fixture.nativeElement.querySelector('#sort-options');
+    expect(dropdownOptions.children.length).toBeGreaterThan(0);
+});
+
+  it('should update component state when a sort option is clicked', () => {    
+    const selectEl = fixture.nativeElement.querySelector('#selectElement');    
+    selectEl.click();
+    fixture.detectChanges();
+
+    const selectOptions = fixture.nativeElement.querySelector('#sort-options');
+    expect(selectOptions.children.length).toEqual(4);
+    selectOptions.children[1].click();
+    fixture.detectChanges();
+
+    // Assert: Check if the component state is updated as expected
+    expect(component.selectedSortBy).toBe('Area'); 
+});
 });
