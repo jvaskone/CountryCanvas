c775dd21366f3366ce7be63ee0c639dd78d6d258
Judit Vasko-Szedlar <129600562+jvaskone@users.noreply.github.com>
2024-05-09 13:05:24 +0200

    junit test added for theme switcher

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,25 +1,28 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { RouterTestingModule } from "@angular/router/testing";
 
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent, RouterTestingModule],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+  it('should create the app', () => {    
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'CountryCanvas' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+  it(`should have the 'CountryCanvas' title`, () => {    
     expect(app.title).toEqual('CountryCanvas');
   });
 
@@ -29,4 +32,17 @@ describe('AppComponent', () => {
   //   const compiled = fixture.nativeElement as HTMLElement;
   //   expect(compiled.querySelector('h1')?.textContent).toContain('Hello, CountryCanvas');
   // });
+
+  it('should toggle darkTheme when the switch is clicked', () => {
+    // Arrange: Retrieve the theme switcher element
+    app.theme = "light-theme";
+    const switchEl = fixture.nativeElement.querySelector('#themeSwitcher');
+
+    // Act: Simulate clicking the switch
+    switchEl.click();
+    fixture.detectChanges();
+
+    // Assert: Check if darkTheme is updated
+    expect(app.theme).toBe("dark-theme"); // Adjust based on your component logic
+});
 });
